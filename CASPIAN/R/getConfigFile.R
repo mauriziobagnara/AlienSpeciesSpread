d7a5cdc4dd3980de77f1f29aa427b6f78af6010e
mauriziobagnara <maurizio.bagnara@gmail.com>
2019-05-01 12:24:19 +0200

    CASPIAN package removed and rda data folder created

--- CASPIAN/R/getConfigFile.R
+++
@@ -1,5 +0,0 @@
-getConfigFile <- function(outputDir){
-  if (file.exists(file.path(outputDir,"ConfigFile.R"))) warning("Existing configuration file has been overwritten")
-  file.copy(from=file.path(system.file("extdata", package="CASPIAN"),"ConfigFile.R"),to=file.path(outputDir,"ConfigFile.R"),overwrite=T)
-  cat("\n Configuration file saved in ",outputDir, " \n")
-  }
